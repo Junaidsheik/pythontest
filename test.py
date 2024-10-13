2b17d3fe249d71aab853c12142db2d35a87fc2b0
Junaid Sheik <jsheik21@gmail.com>
2024-10-12 23:16:34 -0500

    Basics

---
+++ test.py
@@ -0,0 +1,79 @@
+# if age < 18:
+#     print("You are a minor.")
+# elif age == 18:
+#     print("You are an teenager.")
+# else:
+#     print("You are an adult.")
+#
+#
+# age = 50
+# if age < 50:
+#     print("You are old.")
+# elif age == 50:
+#     print("You are a homo.")
+# else:
+#     print("You are a kiddo.")
+#
+#
+# for i in range(10):
+#     print(i)
+
+# for i in range(101):
+#     print(i)
+#
+# animals = ['dog', 'cat', 'horse', 'sheep', 'cow']
+# for animal in animals:
+#     print(animals)
+
+# count = 0
+# while count <= 10:
+#     print(count)
+#     count += 1
+
+# for i in range(100):
+#     if i == 89:
+#         continue
+#     print(i)
+
+
+# def hello (name):
+#     print(f"Hello,{name} ")
+# hello("Moin")
+
+# def num(a, b):
+#     return a + b
+# result = num(1, 2)
+# result1 = num(1, 5)
+# print(result, result1)
+
+# file = open("test.txt","r+")
+# file.write("Hello\n")
+# file.write("Welcome to my World")
+# file.close()
+
+# with open('test.txt','r') as file:
+#     content = file.read()
+#     print(content)
+
+# fruits = ['apple', 'orange', 'pear']
+# print(fruits[2])
+# fruits.append('banana')
+# print(fruits[3])
+# fruits.remove('banana')
+# print(fruits)
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# print(numbers[6])
+#
+# sets = {1,2,3,4,5,6,7,8,9,10}
+# sets.add(5)
+# sets.remove(4)
+# print(sets)
+
+# student = {"name": "Moin", "Age":25, "Location": "Dallas, Texas"}
+# print(student["name"], student["Age"], student["Location"])
+# student_major = 'cybersecurity'
+# print(student_major)
+
+
+
